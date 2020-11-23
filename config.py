e808dfb25b1a74cebc1d6e8b4d5f6bdc3006d76a
佐藤現 <satougen@satougennoMacBook-Pro.local>
2020-11-23 17:09:58 +0900

    設定ファイルの編集

--- config.py
+++ config.py
@@ -9,7 +9,3 @@ SCOPES = ['https://www.googleapis.com/auth/analytics.readonly', 'https://www.goo
 KEY_FILE_LOCATION = os.getenv('KEY_FILE_LOCATION')
 VIEW_ID = os.getenv('VIEW_ID')
 WEB_PROPERTY_ID = os.getenv('WEB_PROPERTY_ID')
-
-# KEY_FILE_LOCATION = './key/heat-map-c334eb548d8b.json'
-# VIEW_ID = '218171958'
-# WEB_PROPERTY_ID = 'UA-138497896-2'
